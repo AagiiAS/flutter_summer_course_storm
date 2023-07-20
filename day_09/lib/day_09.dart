9b63fae018c10eecb4cf447b1c139e2c53ccc3f8
Storm-11 <storm-11@Admin-11s-iMac.local>
2023-07-20 11:07:03 +0800

    add: day_11_flutter

--- day_09/lib/day_09.dart
+++ day_09/lib/day_09.dart
@@ -1,3 +1,30 @@
-int calculate() {
-  return 6 * 7;
+
+class Sprite {
+  void move (int x){
+    this.x = x;
+  }
+  Sprite(String name){
+    this.name = name;
+  }
+  String say(){
+    return name;
+  }
+  String name = 'Sprite';
+  int x = 0;
+  int y = 0;
+  bool isShow = true;
+  int size = 100;
+  int direction = 90;
+
+}
+
+
+void main (){
+  final luca = Sprite('Luca');
+  final Hat = Sprite('Hat');
+  final Kia = Sprite('Kia');
+  print(luca.say());
+  print(Hat.say());
+  print(Kia.say());
+print(luca.name);
 }
