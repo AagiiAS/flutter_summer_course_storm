5c163df05f921d299a781b1f973eaa5ef9b62050
Storm-11 <storm-11@Admin-11s-iMac.local>
2023-07-25 10:18:37 +0800

    add: day_13

---
+++ wordfind_app/lib/welcome_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: Color(0xFFFBF5F2),
+      body: Container(
+        decoration:
+            BoxDecoration(image: DecorationImage(image: AssetImage(back1.png), Bo
+            )
+            ),
+
+      ),
+    );
+  }
+}
