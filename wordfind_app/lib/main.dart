8a4588ccea535dd4c93abad9228bcf19f650b9b2
Storm-11 <storm-11@Admin-11s-iMac.local>
2023-08-01 13:12:12 +0800

    add: inputpage

--- wordfind_app/lib/main.dart
+++ wordfind_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wordfind_app/start_page.dart';
+import 'package:wordfind_app/task_page.dart';
 import 'package:wordfind_app/welcome_page.dart';
 
 void main() {
@@ -7,7 +8,7 @@ void main() {
     MaterialApp(
         title: 'Word Find Game',
         theme: ThemeData(fontFamily: 'Ribeye'),
-        home: StartPage()),
+        home: TaskPage()),
 
   );
 }
--- wordfind_app/lib/start_page.dart
+++ wordfind_app/lib/start_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wordfind_app/Gradient_letter.dart';
 import 'package:wordfind_app/gradient_text.dart';
-
+import 'package:wordfind_app/input_filed.dart';
 
 class StartPage extends StatelessWidget {
   const StartPage({super.key});
@@ -9,16 +9,73 @@ class StartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFFBF5F2),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Image.asset('assets/image/arrow_back.png'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: SizedBox(
+          height: 50,
+          child: Image.asset('assets/image/game_logo.png'),
+        ),
+      ),
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/image/back1.png'),
-            fit: BoxFit.cover,
+              image: AssetImage('assets/image/back2.png'), fit: BoxFit.cover),
+        ),
+        child: Center(
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 50)),
+              Image.asset('assets/image/iCodeGuyHead.png'),
+              Padding(padding: EdgeInsets.only(top: 20)),
+              GradientText('Player Name', 20.0),
+              Padding(padding: EdgeInsets.only(top: 20)),
+              InputField()
+            ],
           ),
         ),
       ),
+      floatingActionButton: StartButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
 
+class StartButton extends StatelessWidget {
+  const StartButton({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 310,
+      height: 60,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [Color(0xFFE86B02), Color(0xFFFA9541)]),
+          borderRadius: BorderRadius.circular(25)),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text(
+          'START',
+          style: TextStyle(
+              fontFamily: 'Nunito', fontSize: 24, fontWeight: FontWeight.w700),
+        ),
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25))),
+      ),
     );
   }
 }
